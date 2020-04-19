5995ed823bb2308d25e5c318849d0e764c5ec8bb
TauhidulHaqueNaime <mdna19@student.bth.se>
2020-04-19 14:01:06 +0200

    Initial Commit

---
+++ src/test/java/Group3/IntegrationTest.java
@@ -0,0 +1,14 @@
+package Group3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/Group3/LockUnlockTest.java
@@ -0,0 +1,14 @@
+package Group3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LockUnlockTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
