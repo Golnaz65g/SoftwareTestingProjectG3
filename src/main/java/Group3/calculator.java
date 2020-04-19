70b9cd19000e9fcd538d87ce872d6deb97806593
Golnaz65g <52709514+Golnaz65g@users.noreply.github.com>
2020-04-19 14:23:45 +0200

    Updated extended calculator app

--- src/main/java/Group3/calculator.java
+++ src/main/java/Group3/calculator.java
@@ -1,41 +1,25 @@
 package Group3;
 
-public class calculator {
-	@Test
-	
+public class calculator {	
 	public int add(int a, int b) {
 		return a+b;
 	}
-	
-	@Test
-	
-	public void subTest() {
-		calculator junit= new calculator();
-		int result= junit.sub(300, 100);
-		assertEquals (200,result);
+	public int sub(int a, int b) {
+		return a-b;
 	}
 	
-	@Test
-	
-	public void mulTest() {
-		calculator junit= new calculator();
-		int result= junit.mul(20, 20);
-		assertEquals (400,result);
+	public int mul(int a, int b) {
+		return a*b;
 	}
 	
-	@Test
-	
-	public void divTest() {
-		calculator junit= new calculator();
-		int result= junit.div(8, 4);
-		assertEquals (2,result);
+	public int div(int a, int b) {
+		if(b==0){
+			throw new IllegalArgumentException("Number can not be divided by zero");
+		}
+		return a/b;
 	}
 	
-	@Test
-	public void squareTest() {
-		calculator junit= new calculator();
-		int result= junit.square(5);
-		assertEquals(25, result);
-		
+	public int square (int x) {
+		return x*x;
 	}
 }
