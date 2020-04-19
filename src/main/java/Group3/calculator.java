fbdfda81abb446f2aceda9ca8af6653e016859f7
Golnaz65g <52709514+Golnaz65g@users.noreply.github.com>
2020-04-19 14:22:16 +0200

    Updated extended calculator project

--- src/main/java/Group3/calculator.java
+++ src/main/java/Group3/calculator.java
@@ -1,7 +1,41 @@
 package Group3;
 
 public class calculator {
+	@Test
+	
 	public int add(int a, int b) {
 		return a+b;
+	}
+	
+	@Test
+	
+	public void subTest() {
+		calculator junit= new calculator();
+		int result= junit.sub(300, 100);
+		assertEquals (200,result);
+	}
+	
+	@Test
+	
+	public void mulTest() {
+		calculator junit= new calculator();
+		int result= junit.mul(20, 20);
+		assertEquals (400,result);
+	}
+	
+	@Test
+	
+	public void divTest() {
+		calculator junit= new calculator();
+		int result= junit.div(8, 4);
+		assertEquals (2,result);
+	}
+	
+	@Test
+	public void squareTest() {
+		calculator junit= new calculator();
+		int result= junit.square(5);
+		assertEquals(25, result);
+		
 	}
 }
